065ebec595a54a739fea5307426e049d2a258a3e
kolesivo <36096601+kolesivo@users.noreply.github.com>
2023-09-15 13:37:21 +0400

    Add files via upload

---
+++ myproject/fpages/__pycache__/admin.cpython-311.py
@@ -0,0 +1,11 @@
+§
+    ÁGdĞ  ã                   ó¼   — d dl mZ d dlmZ d dlmZ d dlmZ  G d„ de¦  «        Zej	         
+                    e¦  «         ej	                             ee¦  «         dS )é    )Úadmin)ÚFlatPageAdmin)ÚFlatPage)Úgettext_lazyc                   ó:   — e Zd Zdddif ed¦  «        dddœffZdS )r   NÚfields)ÚurlÚtitleÚcontentÚsiteszAdvanced options)Úcollapse)Úenable_commentsÚregistration_requiredÚtemplate_name)Úclassesr   )Ú__name__Ú
+__module__Ú__qualname__Ú_Ú	fieldsets© ó    õB   D:\Ğ£Ñ‡ĞµĞ±Ğ°\module-D1.5_Project_Django\myproject\fpages\admin.pyr   r      sH   € € € € € à	Ğ>Ğ?Ğ@Ø	
+ˆĞÑ	Ô	Ø$ğğ!
+ğ !
+ğ 	ğ
+€I€I€Ir   r   N)Údjango.contribr   Údjango.contrib.flatpages.adminr   Údjango.contrib.flatpages.modelsr   Údjango.utils.translationr   r   ÚsiteÚ
+unregisterÚregisterr   r   r   ú<module>r!      s­   ğØ  Ğ  Ğ  Ğ  Ğ  Ğ  Ø 8Ğ 8Ğ 8Ğ 8Ğ 8Ğ 8Ø 4Ğ 4Ğ 4Ğ 4Ğ 4Ğ 4Ø 6Ğ 6Ğ 6Ğ 6Ğ 6Ğ 6ğğ ğ ğ ğ Mñ ô ğ ğ „
+× Ò hÑ Ô Ğ Ø „
+× Ò H˜mÑ ,Ô ,Ğ ,Ğ ,Ğ ,r   
